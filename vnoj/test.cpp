65a32e5449ece50ea3c9e6913cb6f9388237412b
cuong02n <cuong02n@gmail.com>
2023-08-11 06:43:22 +0700

    leetcode

---
+++ vnoj/test.cpp
@@ -0,0 +1,81 @@
+/*
+    author : cuong2905say
+    created : 11-08-2023  00:37:37  UTC: +7
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+template <class T1, class T2>
+ostream& operator<<(ostream& os, pair<T1, T2> A) {
+    os << "{ " << A.first << ", " << A.second << " }";
+    return os;
+}
+template <class T1, class T2, class T3>
+ostream& operator<<(ostream& os, tuple<T1, T2, T3> A) {
+    os << "{ " << get<0>(A) << ", " << get<1>(A) << ", " << get<2>(A) << " }";
+    return os;
+}
+
+template <class it>
+void _print(it begin, it end) {
+    for (it i = begin; i != end; i++) {
+        cout << *i << " ";
+    }
+}
+
+void _verbose() {
+}
+
+int MOD = 1e9 + 7;
+int verbose = -1;
+int all_cases = -1;
+void solve(bool v = false, int all_case = -1) {
+    string x;
+    cin >> x;
+    cout << x << endl;  
+    if (!v && all_case == all_cases) {
+        return;
+    }
+    if (v && all_case == all_cases) {
+        _verbose();
+    }
+}
+
+void reset() {
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    for (int i = 0; i < t; i++) {
+#ifndef ONLINE_JUDGE
+        cout << "case " << i + 1 << ": ";
+#endif
+        if (verbose == i + 1) {
+            solve(true);
+        } else {
+            solve();
+        }
+        reset();
+    }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+#ifndef ONLINE_JUDGE
+    chrono::duration<double> time_span = chrono::duration_cast<std::chrono::duration<double>>(end - start);
+    cout << "time use: " << time_span.count() << endl;
+#endif
+
+    return 0;
+}
+
